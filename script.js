feb12535e4267df65fc18263540397a4513c8ffb
shaktisinh <hunterparmar4384@gmail.com>
2026-07-25 17:51:54 +0530

    page-link-setting

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // for login-form
 
+let send_btn=document.getElementsByClassName('send-btn')
+for(let i=0;i<send_btn.length;i++){
+ send_btn[i].onclick= function () {
+   alert("Your Request has submiited Successfully. we will contact you shortly.")
+}
+ 
+}
+
+
 
 
 
@@ -7,7 +16,8 @@
 
 function register() {
     let reg = document.getElementsByClassName('register')
-    let reg_toggle =document.getElementById('reg-toogle')
+    let reg_toggle =document.getElementById('reg-toggle')
+    let btn=document.getElementById('send-btn')
 
 
         for (let i = 0; i < reg.length; i++) {
@@ -15,22 +25,54 @@ function register() {
 
         }
         
-
+        btn.innerHTML='Register <i class="fa-solid fa-arrow-right ml-2 "></i>'
         reg_toggle.innerHTML='Allready an Account? <button onclick="login()" id="reg-btn" class=" text-sky-500 font-medium">Log in Here</button>'
+
+        
+} 
+
+
+function login(){
+let reg = document.getElementsByClassName('register')
+let reg_toggle =document.getElementById('reg-toggle')
+let btn=document.getElementById('send-btn')
+
+
+        for (let i = 0; i < reg.length; i++) {
+            reg[i].style.display = 'none'
+
+        }
+        
+        btn.innerHTML='Log in <i class="fa-solid fa-arrow-right ml-2 "></i>'
+        document.getElementById('reg-toggle').innerHTML= 'Dont have an Account? <button onclick="register()" id="reg-btn" class=" text-sky-500 font-medium">Register</button>'
 }
 
 
-// function login(){
-// let reg = document.getElementsByClassName('register')
+function fgtpassword(){
 
 
-//         for (let i = 0; i < reg.length; i++) {
-//             reg[i].style.display = 'none'
+}
 
-//         }
-        
 
-//         document.getElementById('reg-toogle').innerHTML= 'Dont have an Account? <button onclick="register()" id="reg-btn" class=" text-sky-500 font-medium">Register</button>'
-// }
+function openPopup(){
 
+    document.getElementById("forgotPopup").classList.remove("hidden");
+    document.getElementById("forgotPopup").classList.add("flex");
+
+}
+
+function closePopup(){
+
+    document.getElementById("forgotPopup").classList.remove("flex");
+    document.getElementById("forgotPopup").classList.add("hidden");
+
+}
+
+function sendLink(){
+
+    alert("Password reset link sent successfully.");
+
+    closePopup();
+
+}
 
